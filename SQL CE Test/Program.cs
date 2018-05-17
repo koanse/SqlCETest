36e97add6c9c2ca940519ce1bd93595428e9a4cf
koanse <ant.kondaurov@gmail.com>
2018-05-18 02:21:24 +0300

    Initial commit.

---
+++ SQL CE Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+
+namespace SQL_CE_Test
+{
+    static class Program
+    {       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
